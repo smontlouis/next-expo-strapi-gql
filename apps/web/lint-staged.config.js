5dfbbe413737db9538b7611aaf3f3e853bd867da
bulby97 <s.montlouis.calixte@gmail.com>
2022-06-07 19:37:44 +0200

    fix: linting issues

--- apps/web/lint-staged.config.js
+++ apps/web/lint-staged.config.js
@@ -1,10 +1,13 @@
+const path = require('path')
+
+const getFilenames = (filenames) =>
+  filenames.map((f) => path.relative(process.cwd(), f))
+
 module.exports = {
   '**/*.(ts|tsx)': () => 'yarn web type-check',
   // Lint then format TypeScript and JavaScript files
   '**/*.(ts|tsx|js)': (filenames) => [
-    `next lint --fix --file ${filenames
-      .map((f) => path.relative(process.cwd(), f))
-      .join(' --file ')}`,
+    `yarn web lint . ${getFilenames(filenames)}`,
     `prettier --write ${filenames.join(' ')}`,
   ],
 
