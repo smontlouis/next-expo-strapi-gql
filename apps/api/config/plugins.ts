a5bc3ab98c1b2c517f31b629809bc4fdfec5ee1f
bulby97 <s.montlouis.calixte@gmail.com>
2022-06-08 12:01:42 +0200

    chore: enable graphql playground

--- apps/api/config/plugins.ts
+++ apps/api/config/plugins.ts
@@ -1,4 +1,14 @@
 export default {
+  graphql: {
+    config: {
+      depthLimit: 15,
+      amountLimit: 10000,
+      playgroundAlways: true,
+      apolloServer: {
+        tracing: true,
+      },
+    },
+  },
   'users-permissions': {
     config: {
       jwt: {
